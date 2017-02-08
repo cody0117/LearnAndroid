d5ce7d14fd6a1453a1b9b98dd7dbc6de37980204
Victor <em3888@gmail.com>
2017-02-08 19:41:29 +0800

    Add 2 more lessons

--- kotlinLangSandBox/src/main/kotlin/inheritance/AbstractClass.kt
+++ kotlinLangSandBox/src/main/kotlin/inheritance/AbstractClass.kt
@@ -6,19 +6,19 @@ package inheritance
 
 abstract class StoredEntity{
     val isActive = true
-    abstract fun status(): String  {
-     return isActive.toString()
-    }
+//    abstract fun status(): String  {
+//     return isActive.toString()
+//    }
 }
 
 class Employee:StoredEntity() {
-    override fun status(): String {
-        throw UnsupportedOperationException("not implemented")
-    }
+//    override fun status(): String {
+//        throw UnsupportedOperationException("not i2mplemented")
+//    }
 }
 
 fun main(args: Array<String>) {
     val se = Employee()
-    se.isAtive
-    se.status()
+    //se.isAtive
+    //se.status()
 }
