6c5d41d58badc7b6f9f73daa21a9fe8976d8dd76
Victor <em3888@gmail.com>
2017-02-10 10:03:57 +0800

    Add Extenstion Function, propert, infix and Lamda function

--- kotlinLangSandBox/src/main/kotlin/functions/ExtensionFunction.kt
+++ kotlinLangSandBox/src/main/kotlin/functions/ExtensionFunction.kt
@@ -1,12 +1,25 @@
 package functions
 
+import java.util.*
+
 /**
  * Created by victor888 on 2/9/2017.
  *
  */
 fun main(args: Array<String>) {
   extendion_function()
+  createAnyNullTypeFunction()
+
 }
+
+fun createAnyNullTypeFunction() {
+  val somenull: Nothing? = null
+  somenull.printnull()
+
+  val someValue = "12"
+  someValue.printnull()
+}
+
 
 fun extendion_function() {
   val ztoaProgression = 'z' downTo 'a' // downTo keyboard are coming from extension method
@@ -14,4 +27,24 @@ fun extendion_function() {
 //  public infix fun Char.downTo(to: Char): CharProgression {
 //    return CharProgression.fromClosedRange(this, to, -1)
 //  }
+
+  val mylist = arrayListOf<Int>(1, 2, 3)
+  println(mylist)
+  mylist.swap(0,2)
+  println(mylist)
+
+
+
+}
+
+fun Any?.printnull()
+{
+  if (this ==null) println("Object is null")
+  else println("Object is ${this.toString()}")
+}
+private fun <E> ArrayList<E>.swap(index: Int, index2: Int) {
+
+  val Temp = this[index]
+  this[index] = this[index2]
+  this[index2]= Temp
 }
---
+++ kotlinLangSandBox/src/main/kotlin/functions/LamdaFunctions.kt
@@ -0,0 +1,8 @@
+package functions
+
+/**
+ * Created by victor888 on 2/10/2017.
+ */
+fun main(args: Array<String>) {
+
+}
